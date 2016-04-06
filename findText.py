b926e6de57b71a26e40a3d5cd72d3f8ce2d5cc5e
Mario Garcia <mayitzin@gmail.com>
2016-04-07 01:50:13 +0200

    Apply Laplacian to input image

--- findText.py
+++ findText.py
@@ -11,10 +11,19 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import scipy.ndimage as scimg
 import numpy as np
+# import skimage.filters.laplace as laplace
 
 # Load image
 page = scimg.imread('page001.png')
 
-# Show image
+# Filter image
+sharpened_page = scimg.filters.laplace(page)
+
+# Show images
+plt.subplot(1,2,1)
 plt.imshow(page)
+plt.subplot(1,2,2)
+plt.imshow(sharpened_page)
+
+# Show image
 plt.show()
