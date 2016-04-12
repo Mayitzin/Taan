624d9f94dafbf82fc315fd960bf0e79c959b83b4
Mayitzin <mayitzin@gmail.com>
2016-04-12 14:47:26 +0200

    Correct threshold for binarization of image

--- findText.py
+++ findText.py
@@ -12,6 +12,7 @@ import matplotlib.image as mpimg
 import scipy.ndimage as scimg
 import scipy.signal as sig
 import numpy as np
+import copy
 
 # Load image in gray-scale
 page = (255-scimg.imread('page001.png', flatten=True))/255.0
@@ -28,21 +29,27 @@ max_sharp = np.max(sharpened_page)
 print np.shape(sharpened_page), "\n", min_sharp, "\n", max_sharp
 
 # Histogram of Image
-hist = scimg.measurements.histogram(sharpened_page, min_sharp, max_sharp, 255)
+hist = scimg.measurements.histogram(sharpened_page, min_sharp, max_sharp, 256)
 
 # Binarization of Image
-threshold = 0.004
-sharpened_page[sharpened_page>threshold] = 1.0
-sharpened_page[sharpened_page<=threshold] = 0.0
-print np.shape(sharpened_page), "\n", np.min(sharpened_page), "\n", np.max(sharpened_page)
+threshold = -0.004
+binary_img = copy.copy(sharpened_page)
+binary_img[binary_img>threshold] = max_sharp
+binary_img[binary_img<=threshold] = min_sharp
+binary_img[binary_img==max_sharp] = 1.0
+binary_img[binary_img==min_sharp] = 0.0
+print np.shape(binary_img), "\n", np.min(binary_img), "\n", np.max(binary_img)
+print 256.0/np.argmax(hist)
 
 # Show images
-plt.subplot(1,3,1)
+plt.subplot(1,4,1)
 plt.imshow(page, cmap='Greys')
-plt.subplot(1,3,2)
-plt.plot(np.linspace(min_sharp, max_sharp, num=255),hist)
-plt.subplot(1,3,3)
+plt.subplot(1,4,2)
 plt.imshow(sharpened_page, cmap='Greys')
+plt.subplot(1,4,3)
+plt.plot(np.linspace(min_sharp, max_sharp, num=256),hist)
+plt.subplot(1,4,4)
+plt.imshow(binary_img, cmap='Greys')
 
 # Show image
 plt.show()
